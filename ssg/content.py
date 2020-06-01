560e4519bc57675ac596ad6deadab89afddbea94
Marcel Comello <marcel@comello.be>
2020-06-01 21:03:56 +0200

    auto-commit

--- ssg/content.py
+++ ssg/content.py
@@ -1,2 +1,27 @@
 import re
 from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimiter = "^(:-|+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
