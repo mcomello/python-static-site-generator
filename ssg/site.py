34ce1f1a9833e41056a1e1c2b630cb4d1cf03c81
marcel <dev@comello.be>
2020-06-01 16:32:28 +0200

    mod 2

--- ssg/site.py
+++ ssg/site.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -12,5 +13,20 @@ class Site:
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
-            if path.is_dir(): self.create_dir(path)
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
 
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if extension == parser.valid_extentions(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+
+        if parser:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
